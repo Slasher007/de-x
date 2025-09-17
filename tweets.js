2f76393b5b73b6542262eb2ff10f9b94444c1f06
Slasher00seven <54632036+Slasher007@users.noreply.github.com>
2025-09-17 09:42:11 +0200

    Create tweets.js

---
+++ tweets.js
@@ -0,0 +1,579 @@
+window.YTD.tweets.part0 = [
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1793687496154587630"
+          ],
+          "editableUntil" : "2024-05-23T17:56:38.000Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : false
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "h",
+            "screen_name" : "itsHesamSheikh",
+            "indices" : [
+              "0",
+              "15"
+            ],
+            "id_str" : "1714573527993442304",
+            "id" : "1714573527993442304"
+          },
+          {
+            "name" : "Yann LeCun",
+            "screen_name" : "ylecun",
+            "indices" : [
+              "16",
+              "23"
+            ],
+            "id_str" : "48008938",
+            "id" : "48008938"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "74"
+      ],
+      "favorite_count" : "2",
+      "in_reply_to_status_id_str" : "1793683199677313429",
+      "id_str" : "1793687496154587630",
+      "in_reply_to_user_id" : "1714573527993442304",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1793687496154587630",
+      "in_reply_to_status_id" : "1793683199677313429",
+      "created_at" : "Thu May 23 16:56:38 +0000 2024",
+      "favorited" : false,
+      "full_text" : "@itsHesamSheikh @ylecun Isn't what he described from point 1 to 4 enough ?",
+      "lang" : "en",
+      "in_reply_to_screen_name" : "itsHesamSheikh",
+      "in_reply_to_user_id_str" : "1714573527993442304"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1539601928145059844"
+          ],
+          "editableUntil" : "2022-06-22T14:01:18.000Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : false
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "𝕋𝕠𝕟𝕦𝕩 #𝟛𝟙𝟛",
+            "screen_name" : "tonux_samb",
+            "indices" : [
+              "0",
+              "11"
+            ],
+            "id_str" : "275957787",
+            "id" : "275957787"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "76"
+      ],
+      "favorite_count" : "0",
+      "in_reply_to_status_id_str" : "1539378479908675585",
+      "id_str" : "1539601928145059844",
+      "in_reply_to_user_id" : "275957787",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1539601928145059844",
+      "in_reply_to_status_id" : "1539378479908675585",
+      "created_at" : "Wed Jun 22 13:31:18 +0000 2022",
+      "favorited" : false,
+      "full_text" : "@tonux_samb Modèle entraîné sur du open-source pour devenir ensuite payant 🤦",
+      "lang" : "fr",
+      "in_reply_to_screen_name" : "tonux_samb",
+      "in_reply_to_user_id_str" : "275957787"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1445427483550097414"
+          ],
+          "editableUntil" : "2021-10-05T17:35:41.479Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : true
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+      "entities" : {
+        "user_mentions" : [
+          {
+            "name" : "Benjamin Code",
+            "screen_name" : "benjamincode",
+            "indices" : [
+              "0",
+              "13"
+            ],
+            "id_str" : "961600873553121285",
+            "id" : "961600873553121285"
+          }
+        ],
+        "urls" : [ ],
+        "symbols" : [ ],
+        "media" : [
+          {
+            "expanded_url" : "https://x.com/Slasher0077/status/1445427483550097414/photo/1",
+            "indices" : [
+              "18",
+              "41"
+            ],
+            "url" : "https://t.co/z2cTOzyNwI",
+            "media_url" : "http://pbs.twimg.com/media/FA8wonkXIAIcpa1.jpg",
+            "id_str" : "1445427479783612418",
+            "id" : "1445427479783612418",
+            "media_url_https" : "https://pbs.twimg.com/media/FA8wonkXIAIcpa1.jpg",
+            "sizes" : {
+              "large" : {
+                "w" : "736",
+                "h" : "719",
+                "resize" : "fit"
+              },
+              "thumb" : {
+                "w" : "150",
+                "h" : "150",
+                "resize" : "crop"
+              },
+              "medium" : {
+                "w" : "736",
+                "h" : "719",
+                "resize" : "fit"
+              },
+              "small" : {
+                "w" : "680",
+                "h" : "664",
+                "resize" : "fit"
+              }
+            },
+            "type" : "photo",
+            "display_url" : "pic.x.com/z2cTOzyNwI"
+          }
+        ],
+        "hashtags" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "41"
+      ],
+      "favorite_count" : "56",
+      "in_reply_to_status_id_str" : "1445404410457305095",
+      "id_str" : "1445427483550097414",
+      "in_reply_to_user_id" : "961600873553121285",
+      "truncated" : false,
+      "retweet_count" : "2",
+      "id" : "1445427483550097414",
+      "in_reply_to_status_id" : "1445404410457305095",
+      "possibly_sensitive" : false,
+      "created_at" : "Tue Oct 05 16:35:41 +0000 2021",
+      "favorited" : false,
+      "full_text" : "@benjamincode Moi https://t.co/z2cTOzyNwI",
+      "lang" : "und",
+      "in_reply_to_screen_name" : "benjamincode",
+      "in_reply_to_user_id_str" : "961600873553121285",
+      "extended_entities" : {
+        "media" : [
+          {
+            "expanded_url" : "https://x.com/Slasher0077/status/1445427483550097414/photo/1",
+            "indices" : [
+              "18",
+              "41"
+            ],
+            "url" : "https://t.co/z2cTOzyNwI",
+            "media_url" : "http://pbs.twimg.com/media/FA8wonkXIAIcpa1.jpg",
+            "id_str" : "1445427479783612418",
+            "id" : "1445427479783612418",
+            "media_url_https" : "https://pbs.twimg.com/media/FA8wonkXIAIcpa1.jpg",
+            "sizes" : {
+              "large" : {
+                "w" : "736",
+                "h" : "719",
+                "resize" : "fit"
+              },
+              "thumb" : {
+                "w" : "150",
+                "h" : "150",
+                "resize" : "crop"
+              },
+              "medium" : {
+                "w" : "736",
+                "h" : "719",
+                "resize" : "fit"
+              },
+              "small" : {
+                "w" : "680",
+                "h" : "664",
+                "resize" : "fit"
+              }
+            },
+            "type" : "photo",
+            "display_url" : "pic.x.com/z2cTOzyNwI"
+          }
+        ]
+      }
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1399256141419405313"
+          ],
+          "editableUntil" : "2021-05-31T07:47:15.693Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : true
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Hardisk",
+            "screen_name" : "Hardisk",
+            "indices" : [
+              "0",
+              "8"
+            ],
+            "id_str" : "211644502",
+            "id" : "211644502"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "24"
+      ],
+      "favorite_count" : "1",
+      "in_reply_to_status_id_str" : "1399255900288917504",
+      "id_str" : "1399256141419405313",
+      "in_reply_to_user_id" : "831558695364460544",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1399256141419405313",
+      "in_reply_to_status_id" : "1399255900288917504",
+      "created_at" : "Mon May 31 06:47:15 +0000 2021",
+      "favorited" : false,
+      "full_text" : "@Hardisk *d'interactions",
+      "lang" : "fr",
+      "in_reply_to_screen_name" : "Slasher007ven",
+      "in_reply_to_user_id_str" : "831558695364460544"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1398614027157266440"
+          ],
+          "editableUntil" : "2021-05-29T13:15:43.723Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : true
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "klaus🇸🇩🇸🇩🇸🇩",
+            "screen_name" : "NdiayeOuz11",
+            "indices" : [
+              "0",
+              "12"
+            ],
+            "id_str" : "893220593927155712",
+            "id" : "893220593927155712"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "14"
+      ],
+      "favorite_count" : "1",
+      "in_reply_to_status_id_str" : "1398252219787579396",
+      "id_str" : "1398614027157266440",
+      "in_reply_to_user_id" : "893220593927155712",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1398614027157266440",
+      "in_reply_to_status_id" : "1398252219787579396",
+      "created_at" : "Sat May 29 12:15:43 +0000 2021",
+      "favorited" : false,
+      "full_text" : "@Ndiayeouz11 😂",
+      "lang" : "qme",
+      "in_reply_to_screen_name" : "NdiayeOuz11",
+      "in_reply_to_user_id_str" : "893220593927155712"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1384897727046242309"
+          ],
+          "editableUntil" : "2021-04-21T16:52:02.940Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : true
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Vincent | Objectif Data Science",
+            "screen_name" : "ObjectifDataSci",
+            "indices" : [
+              "0",
+              "16"
+            ],
+            "id_str" : "1384090711327350784",
+            "id" : "1384090711327350784"
+          },
+          {
+            "name" : "Thread Reader Unroll Helper",
+            "screen_name" : "UnrollHelper",
+            "indices" : [
+              "17",
+              "30"
+            ],
+            "id_str" : "1095406446001369088",
+            "id" : "1095406446001369088"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "30"
+      ],
+      "favorite_count" : "0",
+      "in_reply_to_status_id_str" : "1384829908556386304",
+      "id_str" : "1384897727046242309",
+      "in_reply_to_user_id" : "1384090711327350784",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1384897727046242309",
+      "in_reply_to_status_id" : "1384829908556386304",
+      "created_at" : "Wed Apr 21 15:52:02 +0000 2021",
+      "favorited" : false,
+      "full_text" : "@ObjectifDataSci @UnrollHelper",
+      "lang" : "qam",
+      "in_reply_to_screen_name" : "ObjectifDataSci",
+      "in_reply_to_user_id_str" : "1384090711327350784"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1375796833579073536"
+          ],
+          "editableUntil" : "2021-03-27T14:08:20.851Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : true
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Programmer Humor",
+            "screen_name" : "PR0GRAMMERHUM0R",
+            "indices" : [
+              "0",
+              "16"
+            ],
+            "id_str" : "965774983527043072",
+            "id" : "965774983527043072"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "62"
+      ],
+      "favorite_count" : "1",
+      "in_reply_to_status_id_str" : "1375553165899268102",
+      "id_str" : "1375796833579073536",
+      "in_reply_to_user_id" : "965774983527043072",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1375796833579073536",
+      "in_reply_to_status_id" : "1375553165899268102",
+      "created_at" : "Sat Mar 27 13:08:20 +0000 2021",
+      "favorited" : false,
+      "full_text" : "@PR0GRAMMERHUM0R Some time i shutdown my laptop by compassion.",
+      "lang" : "en",
+      "in_reply_to_screen_name" : "PR0GRAMMERHUM0R",
+      "in_reply_to_user_id_str" : "965774983527043072"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1370856966801735684"
+          ],
+          "editableUntil" : "2021-03-13T22:59:04.833Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : true
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "Mansour_Ibn_babacar",
+            "screen_name" : "babacar_ibn",
+            "indices" : [
+              "0",
+              "12"
+            ],
+            "id_str" : "1528000499252924418",
+            "id" : "1528000499252924418"
+          },
+          {
+            "name" : "Mounira Aboubakr",
+            "screen_name" : "preinomnom",
+            "indices" : [
+              "13",
+              "24"
+            ],
+            "id_str" : "746015678",
+            "id" : "746015678"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "133"
+      ],
+      "favorite_count" : "8",
+      "in_reply_to_status_id_str" : "1370831812092641288",
+      "id_str" : "1370856966801735684",
+      "in_reply_to_user_id" : "1475900618",
+      "truncated" : false,
+      "retweet_count" : "1",
+      "id" : "1370856966801735684",
+      "in_reply_to_status_id" : "1370831812092641288",
+      "created_at" : "Sat Mar 13 21:59:04 +0000 2021",
+      "favorited" : false,
+      "full_text" : "@babacar_ibn @preinomnom Nigeria Amena béneu moukhadamu baye boufa ameu +16 millions de talibé. Moy équivalent ak population Sénégal.",
+      "lang" : "fr",
+      "in_reply_to_user_id_str" : "1475900618"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "1368188461388271618"
+          ],
+          "editableUntil" : "2021-03-06T14:15:23.564Z",
+          "editsRemaining" : "5",
+          "isEditEligible" : true
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [
+          {
+            "name" : "🇵🇸 Sokhna Lily Kone Feu🔥",
+            "screen_name" : "DoctorLove475",
+            "indices" : [
+              "0",
+              "14"
+            ],
+            "id_str" : "762750523",
+            "id" : "762750523"
+          },
+          {
+            "name" : "Califat",
+            "screen_name" : "dipdunduguiss",
+            "indices" : [
+              "15",
+              "29"
+            ],
+            "id_str" : "1193329015794393089",
+            "id" : "1193329015794393089"
+          }
+        ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "0",
+        "66"
+      ],
+      "favorite_count" : "0",
+      "in_reply_to_status_id_str" : "1368164757547712519",
+      "id_str" : "1368188461388271618",
+      "in_reply_to_user_id" : "762750523",
+      "truncated" : false,
+      "retweet_count" : "0",
+      "id" : "1368188461388271618",
+      "in_reply_to_status_id" : "1368164757547712519",
+      "created_at" : "Sat Mar 06 13:15:23 +0000 2021",
+      "favorited" : false,
+      "full_text" : "@DoctorLove475 @dipdunduguiss Pourquoi son compte a été suspendu ?",
+      "lang" : "fr",
+      "in_reply_to_screen_name" : "DoctorLove475",
+      "in_reply_to_user_id_str" : "762750523"
+    }
+  }
+]
